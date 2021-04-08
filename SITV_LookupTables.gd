03393d81d0c92d8c44fa98e121af9356a9612716
ScorpionInc <master763763763@yahoo.com>
2021-04-08 19:15:43 -0400

    Update SITV_LookupTables.gd

--- SITV_LookupTables.gd
+++ SITV_LookupTables.gd
@@ -1,4 +1,21 @@
 extends Node
 
+#The Transvoxel Algorithm look-up tables
+#The following data originates from Eric Lengyel's Transvoxel Algorithm.
+#Lengyel, Eric. “Voxel-Based Terrain for Real-Time Virtual Simulations”. PhD diss., University of California at Davis, 2010
+
+#Variables
+var regularCellClass
+var regularCellData
+var regularVertexData
+var transitionCellClass
+
+#Overrides/Implements
 func _ready():
   pass
+
+#Functions
+
+#Methods
+
+#Events / Signals
