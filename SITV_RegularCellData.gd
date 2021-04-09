c60ddf6fa4e42d3373a72be3bc60c6b7c9a52860
ScorpionInc <master763763763@yahoo.com>
2021-04-08 23:43:22 -0400

    Update SITV_RegularCellData.gd

--- SITV_RegularCellData.gd
+++ SITV_RegularCellData.gd
@@ -1,13 +1,23 @@
 extends Node
 class_name RegularCellData
 
-#Variables
+#The RegularCellData structure holds information about the triangulation
+#used for a single equivalence class in the modified Marching Cubes algorithm,
+#described in Section 3.2.
 
-#Overrides/Implements
-func _ready():
-  self.set_process(false)
+#Variables
+var geometryCounts:int    # High nibble is vertex count, low nibble is triangle count.
+var vertexIndex:Array     # Groups of 3 indexes giving the triangulation.
+
+#Overrides/Implementations
+func _ready() -> void:
+	self.set_process(false)
 
 #Functions
+func GetVertexCount() -> int:
+	return (geometryCounts >> 4)
+func GetTriangleCount() -> int:
+	return (geometryCounts & 0x0F)
 
 #Methods
 
