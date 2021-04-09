9b2d34869b637deaff2b36e7e7ef1debe601e8c3
ScorpionInc <master763763763@yahoo.com>
2021-04-09 00:24:17 -0400

    Update and rename SITV_RegularCellData.gd to SITV_RTCellData.gd

--- SITV_RTCellData.gd
+++ SITV_RTCellData.gd
@@ -1,9 +1,13 @@
 extends Node
-class_name RegularCellData
+class_name RTCellData
 
 #The RegularCellData structure holds information about the triangulation
 #used for a single equivalence class in the modified Marching Cubes algorithm,
 #described in Section 3.2.
+
+# The TransitionCellData structure holds information about the triangulation
+# used for a single equivalence class in the Transvoxel Algorithm transition cell,
+# described in Section 4.3.
 
 #Variables
 var geometryCounts:int    # High nibble is vertex count, low nibble is triangle count.
@@ -11,10 +15,11 @@ var vertexIndex:Array     # Groups of 3 indexes giving the triangulation.
 
 #Overrides/Implementations
 func _init( b:int, a:Array ) -> void:
-	geometryCounts = b
+	self.geometryCounts = b
 	self.vertexIndex = a.duplicate(true)#duplicate(deep)
 func _ready() -> void:
 	self.set_process(false)
+	pass
 
 #Functions
 func GetVertexCount() -> int:
